fca49185d63546ed69a836c6c20ca9cd44d6614a
kovalenk_art <yura.kovalenko.1996.29@g,ail.com>
2018-09-18 19:03:33 +0300

    Prettier hook test 2

--- HackerNews/src/app/app.component.ts
+++ HackerNews/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {SwUpdate} from '@angular/service-worker';
+import {DataService} from './data.service';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+           time: any;
+  joke: any;
+            str: any;
+        myUrl: any;
+
   title = 'HackerNews';
+
+  constructor(updates: SwUpdate, private data: DataService, private http: HttpClient, private Test1: any, private Test2: any) {
+    updates.available.subscribe(event => {
+
+      // this.update = true;
+      updates.activateUpdate().then(() => document.location.reload());
+
+    });
+  }
+
+  // ngOnInit() {
+  //   this.data.gimmeJokes().subscribe(res => {
+  //     this.str = `https://hacker-news.firebaseio.com/v0/item/${res[3]}.json?print=pretty`;
+  //     this.joke = this.http.get(`https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`);
+  //     this.data.getData(this.str).subscribe(rez => {
+  //       this.joke = rez;
+  //       this.myUrl = ' // ' + rez.url.split('/')[2];
+  //       console.log(this.myUrl);
+  //       this.time = this.getNormTime(rez.time);
+  //     });
+  //     // this.joke = res;
+  //   });
+  // }
+  // getNormTime(num: number) {
+  //   const Now = Math.round(new Date().getTime() / 1000.0);
+  //   const Mydate = new Date(num * 1000);
+  //   const diff = Now - num;
+  //   if (Math.floor(diff / (3600 * 24)) > 0) {
+  //     if (Math.floor(diff / (3600 * 24)) === 1) {
+  //       return 'a day ago';
+  //     } else {
+  //       return Math.floor(diff / (3600 * 24)) + ' days ago';
+  //     }
+  //   } else {
+  //     if (Math.floor(diff / 3600) > 0) {
+  //       if (Math.floor(diff / 3600) === 1) {
+  //         return 'an hour ago';
+  //       } else {
+  //         return Math.floor(diff / 3600) + ' hours ago';
+  //       }
+  //     } else {
+  //       if (Math.floor(diff / 60) === 1) {
+  //         return 'a minute ago';
+  //       } else {
+  //         return Math.floor(diff / 60) + ' minutes ago';
+  //       }
+  //     }
+  //   }
+  // }
 }
