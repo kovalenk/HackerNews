832cca04529285d4021839a7f50437357c09762c
kovalenk_art <yura.kovalenko.1996.29@g,ail.com>
2018-09-18 19:40:46 +0300

    Prettier test 6

--- HackerNews/src/PrettierTest.js
+++ HackerNews/src/PrettierTest.js
@@ -1,30 +1,59 @@
 function __asyncDelegator(o) {
   var i, p;
-  return i = {}, verb("next"), verb("throw", function (e) { throw e; }), verb("return"), i[Symbol.iterator] = function () { return this; }, i;
-  function verb(n, f) { i[n] = o[n] ? function (v) { return (p = !p) ? { value: __await(o[n](v)), done: n === "return" } : f ? f(v) : v; } : f; }
+  return (
+    (i = {}),
+    verb("next"),
+    verb("throw", function(e) {
+      throw e;
+    }),
+    verb("return"),
+    (i[Symbol.iterator] = function() {
+      return this;
+    }),
+    i
+  );
+  function verb(n, f) {
+    i[n] = o[n]
+      ? function(v) {
+          return (p = !p)
+            ? { value: __await(o[n](v)), done: n === "return" }
+            : f
+              ? f(v)
+              : v;
+        }
+      : f;
+  }
 }
 
 function __asyncValues(o) {
-  if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
-  var m = o[Symbol.asyncIterator], i;
-  return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
-  function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
-  function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
-}
-
-function __makeTemplateObject(cooked, raw) {
-  if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
-  return cooked;
-};
-
-function __importStar(mod) {
-  if (mod && mod.__esModule) return mod;
-  var result = {};
-  if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-  result.default = mod;
-  return result;
-}
-
-function __importDefault(mod) {
-  return (mod && mod.__esModule) ? mod : { default: mod };
+  if (!Symbol.asyncIterator)
+    throw new TypeError("Symbol.asyncIterator is not defined.");
+  var m = o[Symbol.asyncIterator],
+    i;
+  return m
+    ? m.call(o)
+    : ((o =
+        typeof __values === "function" ? __values(o) : o[Symbol.iterator]()),
+      (i = {}),
+      verb("next"),
+      verb("throw"),
+      verb("return"),
+      (i[Symbol.asyncIterator] = function() {
+        return this;
+      }),
+      i);
+  function verb(n) {
+    i[n] =
+      o[n] &&
+      function(v) {
+        return new Promise(function(resolve, reject) {
+          (v = o[n](v)), settle(resolve, reject, v.done, v.value);
+        });
+      };
+  }
+  function settle(resolve, reject, d, v) {
+    Promise.resolve(v).then(function(v) {
+      resolve({ value: v, done: d });
+    }, reject);
+  }
 }
